95af184273704d7b126e58cbffa5488f01f4a692
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-11-21 00:12:14 +0900

    Change n click necessary for skip

--- src/components/BlackOverlay.tsx
+++ src/components/BlackOverlay.tsx
@@ -9,7 +9,7 @@ export const BlackOverlay: React.FC<Props> = ({ children, done }) => {
   return (
     <div
       className={`w-[100vw] h-[100vh] fixed top-0 left-0 bg-[#0C0C0C] transition-[z-index] z-10 transition-opacity duration-[1.5s] ${
-        done || count > 3 ? "opacity-0 z-[-1]" : "opacity-100"
+        done || count > 0 ? "opacity-0 z-[-1]" : "opacity-100"
       }`}
       onClick={() => setCount((count) => count + 1)}
     >
