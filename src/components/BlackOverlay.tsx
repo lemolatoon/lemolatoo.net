eeac81115cef3ef83d1e392605740a0d1606efec
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-11-20 23:47:45 +0900

    Fix

--- src/components/BlackOverlay.tsx
+++ src/components/BlackOverlay.tsx
@@ -7,8 +7,8 @@ type Props = {
 export const BlackOverlay: React.FC<Props> = ({ children, done }) => {
   return (
     <div
-      className={`w-[100vw] h-[100vh] fixed top-0 left-0 bg-[#0C0C0C] z-10 transition-opacity duration-[2s] ${
-        done ? "opacity-0" : "opacity-100"
+      className={`w-[100vw] h-[100vh] fixed top-0 left-0 bg-[#0C0C0C] transition-[z-index] z-10 transition-opacity duration-[2s] ${
+        done ? "opacity-0 z-[-1]" : "opacity-100"
       }`}
     >
       {children}
