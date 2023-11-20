7c9e52478e37beb4c25b503b295641cc332f49ec
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-11-21 00:06:36 +0900

    Add skip by clicks

--- src/components/BlackOverlay.tsx
+++ src/components/BlackOverlay.tsx
@@ -1,15 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 type Props = {
   children: React.ReactNode;
   done: boolean;
 };
 export const BlackOverlay: React.FC<Props> = ({ children, done }) => {
+  const [count, setCount] = React.useState(0);
   return (
     <div
-      className={`w-[100vw] h-[100vh] fixed top-0 left-0 bg-[#0C0C0C] transition-[z-index] z-10 transition-opacity duration-[2s] ${
-        done ? "opacity-0 z-[-1]" : "opacity-100"
+      className={`w-[100vw] h-[100vh] fixed top-0 left-0 bg-[#0C0C0C] transition-[z-index] z-10 transition-opacity duration-[1.5s] ${
+        done || count > 3 ? "opacity-0 z-[-1]" : "opacity-100"
       }`}
+      onClick={() => setCount((count) => count + 1)}
     >
       {children}
     </div>
